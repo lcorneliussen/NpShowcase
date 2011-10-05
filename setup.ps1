78b7f51e397b0f537e0491f093cd3c3eedc6fbdf
Lars Corneliussen <me@lcorneliussen.de>
2011-10-05 23:45:25 +0200

    Delete/restore tags and branches pr. component

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,14 @@
 cls
+
 $root = Split-Path (Resolve-Path $myInvocation.MyCommand.Path);
 $svnRepo = Join-Path $root "svn-repository"
 $checkout = Join-Path $root "checkout"
 $svnUrl = 'file:///' + $svnRepo.Replace("\", "/")
 $mvnRepo = Join-Path $root "mvn-repository"
 $localRepoNpShowcase =  Join-Path $Env:Home .m2\repository\NpShowcase
+
+echo "### Cleanup ###"
+echo ""
 
 # Cleanup SVN stuff
 if (test-path $svnRepo -pathType container) {
@@ -15,6 +19,10 @@ if (test-path $svnRepo -pathType container) {
   gci $checkout -include .svn -r -force | 
    Remove-Item -r -force
 }
+
+echo "# deleting tags and branches in $checkout"
+  gci $checkout -include tags,branches -r -force | 
+   Remove-Item -r -force
 
 # Cleanup staging Maven-Repository stuff
 if (test-path $mvnRepo -pathType container) {
@@ -28,6 +36,8 @@ if (test-path $localRepoNpShowcase -pathType container) {
   del $localRepoNpShowcase -r -force
 }
 
+echo "### Setup ###"
+echo ""
 echo "# creating mvn-repository"
 mkdir $mvnRepo | out-null
 
@@ -40,6 +50,16 @@ svn checkout $svnurl $checkout
 
 echo "# switch to checkout dir"
 cd $checkout
+
+echo "Create branch and tag base dirs"
+gci $checkout | 
+   ? {$_.PsIsContainer} | 
+   %{ 
+     mkdir $(Join-Path $_ branches) | out-null
+     mkdir $(Join-Path $_ tags) | out-null
+   }
+
+echo "initially commit everything" 
 svn add *
 svn commit -m "Initial commit"
 svn status
